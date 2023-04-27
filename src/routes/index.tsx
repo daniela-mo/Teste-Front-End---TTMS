ff5285a26f0f4444ee600b2bcf0ffce7bf63828e
philos-tec <danni.oliver94@gmail.com>
2023-04-26 23:15:01 -0300

    feat: Adicionando temas Light e Dark

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,21 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
+
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<p>Pagina inicial</p>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Teste
+          </Button>
+        }
+      />
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
